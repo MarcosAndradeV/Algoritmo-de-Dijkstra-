b562a2e94aec1c34bb6d64d6851231c25b6fb67f
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-09-25 08:22:21 -0300

    Update main.go

--- main.go
+++ main.go
@@ -5,10 +5,9 @@ type Pair struct {
 }
 
 type Grafo struct {
-	V int
 	Adj [][]Pair
 }
 
 func (g *Grafo) AddAresta(v1 int, v2 int, custo int) {
-	g.Adj[v1] = append(&g.Adj[v1], Pair{v2, custo});
+	g.Adj[v1] = append(g.Adj[v1], Pair{v2, custo});
 }
